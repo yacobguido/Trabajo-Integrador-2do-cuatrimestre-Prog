7b69768880a31310426104145cbcf697de866b4d
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-05-11 19:56:48 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,26 +1,48 @@
 import { Gestor, getPeople } from "./Gestor";
 const student = getPeople();
-
 const gestor = new Gestor(student);
+class consola{
+    setconsole(){
+        const readLineSync = require("readline-sync");
+        console.log('Opciones a seleccionar:\nnumero 1: NuevoAlumno\nnumero 2: NuevoProfesor\nnumero 3: Alumnos\nnumero 4: Profesores\nnumero 5: BuscarAlumno\nnumero 6: BuscarProfesor\nnumero 7: ProfesoresPorAlumno\nnumero 8: AlumnosPorProfesor\nnumero 9: PromedioAlumnos')
+        const seleccionar :string= readLineSync.question("ingrese numero de la funcion a desarrollar: ");
 
-// gestor.setAlumno();
-// gestor.setProfesor();
+        switch (seleccionar) {
+            case "1":
+                gestor.setAlumno();
+                break;
+            case "2":
+                gestor.setProfesor();
+                break;
+            case "3":
+                gestor.getAllAlumnos();
+                break;
+            case "4":
+                gestor.getProfesores();
+                break;
+            case "5":
+                gestor.getAlumno('Cepeda');
+                break;
+            case "6":
+                gestor.getProfesor('Fernandez');
+                break;
+            case "7":
+                gestor.getProfesoresPorAlumno('Bellusci');
+                break;
+            case "8":
+                gestor.getAlumnosPorProfesor('Alfonsin');
+                break;
+            case "9":
+                gestor.getPromedioAlumnos();
+                break;
+            default:
+                console.log("el numero ingresado no corresponde a ninguna opcion")
+                 break;
+            
+        }
+    
+    }
+}
 
-// gestor.getAllAlumnos();
-// gestor.getPromedioAlumnos();
-// gestor.getMateriasAlumno('Cepeda');
-// gestor.getPromedioAlumno('Yacob');
-// gestor.getAlumno('Bellusci')
-// console.table(student)
-// gestor.getProfesor('Fernandez');
-// gestor.getProfesores();
-// gestor.getAlumnosPorProfesor('Alfonsin');
-gestor.getProfesoresPorAlumno('Yacob');
-
-
-
-
-
-
-
-
+const Consola =new consola();
+Consola.setconsole();
