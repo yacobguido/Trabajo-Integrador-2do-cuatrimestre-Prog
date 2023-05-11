833452fb7abd215c6a8d32a7716f6849601e9aee
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-05-11 19:56:03 -0300

    Update Profesor.ts

--- Profesor.ts
+++ Profesor.ts
@@ -5,10 +5,20 @@ import { Persona } from './Persona'
 export class Profesor extends Persona{
     private materia: string;
     private contrato: Contrato
-    public constructor(name: string, lastName: string, dni: number, email: string, materia: string, contrato: Contrato){
+    public constructor(name: string, lastName: string, dni: number, email: string, materia: string,){
         super(name, lastName, dni, email)
         this.materia = materia;
-        this.contrato = contrato;
+        this.contrato = [];
+    }
+    public setContrato(contrato: Contrato){
+        this.contrato.push(contrato);
+    }
+    public getContrato(): Contrato[] {
+        console.log(this.contrato)
+        return this.contrato;
+    }
+    public getLastName(): string {
+        return super.getLastName();
     }
 
 }
